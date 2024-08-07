c6dab9812c86d21497aee089039f4043057517af
Daniel Wu <danielw72908@gmail.com>
2024-08-07 14:46:04 -0400

    Initialized react

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
+
+// Gives permission to your computer to run requests
+app.use(cors());
 
 const db = require("./models");
 
 // Routers
-const postRouter = require("./routes/Posts")
-app.use("/posts", postRouter)
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
